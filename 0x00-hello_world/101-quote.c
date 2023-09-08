8b034694f855e081a5e8be19bdf7d5f1c6e33b65
Dproph <krisbjamb.com>
2023-09-08 06:35:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h> 
+#include <unistd.h>
+
+/**
+*main - Description
+*
+*Return: 1 means (Success)
+*/
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
