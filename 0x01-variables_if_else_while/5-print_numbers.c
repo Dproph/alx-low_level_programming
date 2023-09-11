6dd00610dbac1f1afd7db26dc0ccd51f1cd56213
Dproph <krisbjamb.com>
2023-09-11 06:35:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing all single digit numbes from 0-9
+ *
+ * Return: always (0)
+*/
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
