df089f3cd38ff06dfe85ea6211eeee7be301d6fa
Dproph <krisbjamb.com>
2023-09-12 09:10:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar and a new line
+ *
+ * Return: Success (0)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
