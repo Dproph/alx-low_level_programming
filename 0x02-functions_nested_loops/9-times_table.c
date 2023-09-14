6f0621b08fd68df1a9ac21afa1cfe846d4e1d7d8
Dproph <krisbjamb.com>
2023-09-13 19:43:54 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,28 @@
  */
 void times_table(void)
 {
-	int row, col;
+	int row, col, product;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 9; col++)
 		{
+			product = row * col;
+
 			if (col != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if ((row * col) <= 9 && col != 0)
+			if (product <= 9)
 			{
 				_putchar(' ');
+				_putchar('0' + product);
 			}
-			if (row * col <= 9)
+			else
 			{
-				_putchar('0' + ((row * col) / 10));
-				_putchar('0' + ((row * col) % 10));
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 		}
 		_putchar('\n');
