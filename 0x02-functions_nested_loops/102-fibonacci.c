580ba0383f1104384581cae2f2d4001ed633478d
Dproph <krisbjamb.com>
2023-09-14 01:25:58 -0700

    fibanacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-
-/* Prototype for the _putchar function */
-int _putchar(char c);
 
 /**
  * main - Entry point of the program
@@ -14,35 +10,20 @@ int _putchar(char c);
  */
 int main(void)
 {
-    long int a = 1, b = 2, next;
-    int count;
+	long int a = 1, b = 2, next;
+	int count;
 
-    /* Print the first two Fibonacci numbers */
-    _putchar('1');
-    _putchar(',');
-    _putchar(' ');
-    _putchar('2');
+	printf("%ld, %ld", a, b);
 
-    /* Calculate and print the remaining Fibonacci numbers */
-    for (count = 2; count < 50; count++)
-    {
-        next = a + b;
-        _putchar(',');
-        _putchar(' ');
-        printf("%ld", next);
-        a = b;
-        b = next;
-    }
+	for (count = 2; count < 50; count++)
+	{
+		next = a + b;
+		printf(", %ld", next);
+		a = b;
+		b = next;
+	}
 
-    /* Print a newline character */
-    _putchar('\n');
+	printf("\n");
 
-    return (0);
+	return (0);
 }
-
-/* Definition of the _putchar function */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
