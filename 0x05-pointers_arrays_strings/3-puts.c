2632258eaac5e96bac1103d77bd7baa0129ac277
Dproph <krisbjamb.com>
2023-09-18 07:59:37 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to a string
+ */
+void _puts(char *str)
+{
+	if (str != NULL) {
+	while (*str != '\0') {
+	write(1, str, 1);
+	str++;
+	}
+	write(1, "\n", 1);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to a string
+ */
+void print_rev(char *s)
+{
+	if (s != NULL) {
+	int length = 0;
+
+	while (*s != '\0') {
+	length++;
+	s++;
+	}
+
+	while (length > 0) {
+	s--;
+	write(1, s, 1);
+	length--;
+	}
+
+	write(1, "\n", 1);
+	}
+}
