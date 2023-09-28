63b08cb9bebf0b50b67b1110e2078ad78a6f7fdb
Dproph <krisbjamb.com>
2023-09-28 02:48:12 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,10 @@
 
 /* Function prototype for _puts_recursion */
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
 
 /* Function prototype for _putchar */
 int _putchar(char c);
